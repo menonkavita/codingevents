6b79f80dad51cd5cb53cb29dd687ea1e8d261e46
bluzL <menon_kavita@yahoo.com>
2024-10-06 23:35:39 -0500

    Adding a Form Handler Method

--- src/main/java/or/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/or/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package or.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +14,16 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> eventsList = new ArrayList<>();
+
     // Available @ localhost:8080/events -- not rendering as of now
+    @GetMapping
     public String displayEvents(Model model){
-        List<String> eventsList = new ArrayList<>();
-        eventsList.add("Dancing");
-        eventsList.add("Singing");
-        eventsList.add("Cooking");
+//        List<String> eventsList = new ArrayList<>();
+//        eventsList.add("Dancing");
+//        eventsList.add("Singing");
+//        eventsList.add("Cooking");
+//        model.addAttribute("events", eventsList);
 
         model.addAttribute("events", eventsList);
         return "events/index";
@@ -25,8 +31,15 @@ public class EventController {
 
     // localhost:8080/events/create
     @GetMapping("create")
-    public String createEvents(){
+    public String renderCreateEvents(){
         return "events/create";
+    }
+
+    // localhost:8080/events/create
+    @PostMapping("/create")
+    public String createEvent (@RequestParam String eventName){
+        eventsList.add(eventName);
+        return "redirect:/events";
     }
 }
 
--- src/main/java/or/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/or/launchcode/codingevents/controllers/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+// Accidentally typed in package name as 'or' instead of 'org'
+
 @Controller
 public class HomeController {
 
