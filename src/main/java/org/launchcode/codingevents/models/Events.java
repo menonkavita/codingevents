35f1d53b8a109c1f921fc02c47aed3a6f983e1c8
bluzL <menon_kavita@yahoo.com>
2024-10-12 23:12:38 -0500

    Chapter 14: Models & Data

--- src/main/java/org/launchcode/codingevents/models/Events.java
+++ src/main/java/org/launchcode/codingevents/models/Events.java
@@ -1,12 +1,19 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Events {
+    private int id;
+    private static int nextId=1;
+
     private String name;
     private String description;
 
     public Events(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -25,9 +32,25 @@ public class Events {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Events events = (Events) o;
+        return id == events.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
